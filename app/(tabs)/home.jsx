3257e6aacd2880a886f30c76ab8793ee600b1fb3
Ganesh Shinde <sindheganesh2004@gmail.com>
2024-07-23 15:56:50 +0530

    Initial setup

---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View>
+      <Text style={{fontSize:40, fontFamily: 'outfit-bold'}}>Home</Text>
+    </View>
+  )
+}
